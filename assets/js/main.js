626682f5d1a051aa2492dfa575a21c07f19f680b
Your Name <amr7hmed@gmail.com>
2022-02-14 11:15:32 +0200

    Finsh Front End Deisen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,64 +1,60 @@
+const Imgshow = document.querySelector(".preview-image");
 
-
-/*
-zoomin
-zoomout
-*/
-
-
-const Imgshow = document.querySelector('.preview-image');
-function zoom(event) {
-    event.preventDefault();
-    if(event.deltaY < 0){
-             console.log('Zoom In');
-         }else {
-             console.log('Zoom Out');
-    }
-  }
-  
-Imgshow.onwheel = zoom;
-
+// === Zoom In || Zoom Out In Click The Button
 
 let zoomin = document.getElementById("zoomin"),
-    zoomout = document.getElementById("zoomout"),
-    scale = 1;
+  zoomout = document.getElementById("zoomout"),
+  scale = 1;
 
-function ZoomIn() {
-  if(scale==5){
-    zoomin.style.cursor="none";
-  }else{
-    //Imgshow.style.transformOrigin = event.offsetX + 'px ' + event.offsetY + 'px';  
+function ZoomIn_button() {
+  if (scale == 5) {
+    zoomin.style.cursor = "none";
+    zoomout.style.cursor = "default";
+  } else {
     Imgshow.style.transform = `scale(${++scale})`;
-    zoomin.style.cursor="zoomout";
+    zoomout.style.cursor = "default";
   }
 }
 
-function ZoomOut() {
-    if(scale==1){
-        zoomout.style.cursor="none";
+function ZoomOut_button() {
+  if (scale == 1) {
+    zoomout.style.cursor = "none";
+    zoomin.style.cursor = "default";
+  } else {
+    Imgshow.style.transform = `scale(${--scale})`;
+    zoomin.style.cursor = "default";
+  }
+}
+
+zoomin.addEventListener("click", ZoomIn_button);
+
+zoomout.addEventListener("click", ZoomOut_button);
+
+// === Zoom In && Zoom Out In Click The Crtl Button + The Mouse Scroil
+
+function zoom__mouse(event) {
+  event.preventDefault();
+  const img = event.target;
+  if (event.deltaY < 0) {
+    if (scale == 5) {
+      img.style.transform = `scale(${5})`;
+      zoomin.style.cursor = "none";
+      zoomout.style.cursor = "default";
+    } else {
+      img.style.transformOrigin = event.offsetX + "px " + event.offsetY + "px";
+      img.style.transform = `scale(${++scale})`;
+      zoomout.style.cursor = "default";
+    }
+  } else {
+    if(scale == 1){
+      img.style.transform = `scale(${1})`;
+      zoomout.style.cursor = "none";
+      zoomin.style.cursor = "default";
     }else{
-      //Imgshow.style.transformOrigin = event.offsetX + 'px ' + event.offsetY + 'px';  
-      Imgshow.style.transform = `scale(${--scale})`;
-      zoomin.style.cursor="default";
+      img.style.transform = `scale(${--scale})`;
+      zoomin.style.cursor = "default";
     }
   }
-
-zoomin.addEventListener('click', ZoomIn);
-
-zoomout.addEventListener('click', ZoomOut);
-
-
-
-// ================
-
-function moveReady(e) {
-    e.preventDefault();
-    Imgshow.style.transformOrigin = event.offsetX + 'px ' + event.offsetY + 'px';  
 }
 
-Imgshow.addEventListener("mouseover", () => {
-    Imgshow.style.cursor = "grab";
-  });
-
-
-Imgshow.addEventListener("mousedown", moveReady);
+Imgshow.onwheel = zoom__mouse;
